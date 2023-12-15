20c9ead16cf9d625bb7c0c93e0fb98def82f04e6
Christophe Massolin <christophe.massolin@gmail.com>
2023-12-15 02:39:40 +0000

    feat: Add data-filename attribute

--- .lume/config.ts
+++ .lume/config.ts
@@ -5,11 +5,34 @@ import "npm:prismjs@1.29.0/components/prism-typescript.js";
 import "npm:prismjs@1.29.0/components/prism-bash.js";
 import "npm:prismjs@1.29.0/components/prism-yaml.js";
 import "npm:prismjs@1.29.0/components/prism-json.js";
+import "npm:prismjs@1.29.0/components/prism-markdown.js";
 
-
-const site = lume({ dest: `.lume/_site` });
-
-site.copy("content/assets", "assets")
+const site = lume({
+  dest: `.lume/_site`,
+});
 site.use(blog());
+site.copy("content/assets", "assets");
+
+site.process([".html"], (pages) => {
+  pages.forEach((page) => {
+    page.document!.querySelectorAll("pre code").forEach((element) => {
+      const file = element.getAttribute("data-filename");
+      if (file) {
+        const div = page.document!.createElement("div");
+        div.textContent = `# ${file}`;
+        div.setAttribute(
+          "style",
+          [
+            "opacity: 0.5",
+            "font-style: italic",
+            "padding-bottom: 1em",
+            "font-size: 1em",
+          ].join(";"),
+        );
+        element.prepend(div);
+      }
+    });
+  });
+});
 
 export default site;
