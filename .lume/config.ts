efa698bd98f11c64ea1f94c04662487f46ac7e07
Christophe Massolin <christophe.massolin@gmail.com>
2024-08-20 13:34:53 +0200

    feat(lume): upgrade lume to 2.2.4

--- .lume/config.ts
+++ .lume/config.ts
@@ -3,49 +3,62 @@ import theme from "theme/mod.ts";
 import markdownItBlockquote from "./plugins/markdownItBlockquote.ts";
 import titleFromFilename from "./plugins/titleFromFilename.ts";
 
-const site = lume({ dest: `.lume/_site` })
-  .use(theme({
-    shikiji: {
-      highlighter: {
-        themes: ["github-dark", "github-light"],
-        langs: ["javascript", "yaml", "markdown", "bash", "json", "typescript", "ini"],
-      },
-      themes: {
-        dark: "github-dark",
-        light: "github-light",
-      },
-      cssFile: "/styles/shikiji.css",
-      useColorScheme: true,
-    },
-  }))
-  .use(markdownItBlockquote())
-  .use(titleFromFilename())
-  .copy("favicons", ".")
-  .copy("assets")
-  .remoteFile("favicon.png", import.meta.resolve("./theme/favicons/favicon.png"));
+const site = lume({ dest: ".lume/_site" })
+	.use(
+		theme({
+			shikiji: {
+				highlighter: {
+					themes: ["github-dark", "github-light"],
+					langs: [
+						"javascript",
+						"yaml",
+						"markdown",
+						"bash",
+						"json",
+						"typescript",
+						"ini",
+					],
+				},
+				themes: {
+					dark: "github-dark",
+					light: "github-light",
+				},
+				cssFile: "/styles/shikiji.css",
+				useColorScheme: true,
+			},
+		}),
+	)
+	.use(markdownItBlockquote())
+	.use(titleFromFilename())
+	.copy("favicons", ".")
+	.copy("assets")
+	.remoteFile(
+		"favicon.png",
+		import.meta.resolve("./theme/favicons/favicon.png"),
+	);
 
 const myFiles = [
-  "_data/i18n.yml",
-  "_data.yml",
-  "assets/dark.svg",
-  "assets/light.svg",
-  "favicons/apple-touch-icon-60x60.png",
-  "favicons/apple-touch-icon-76x76.png",
-  "favicons/apple-touch-icon-114x114.png",
-  "favicons/apple-touch-icon-120x120.png",
-  "favicons/apple-touch-icon-144x144.png",
-  "favicons/apple-touch-icon-180x180.png",
-  "favicons/apple-touch-icon.png",
-  "favicons/favicon-32.png",
-  "favicons/favicon-64.png",
-  "favicons/favicon-96.png",
-  "favicons/favicon-196.png",
-  "favicons/favicon.ico",
-  "favicons/favicon.png",
+	"_data/i18n.yml",
+	"_data.yml",
+	"assets/dark.svg",
+	"assets/light.svg",
+	"favicons/apple-touch-icon-60x60.png",
+	"favicons/apple-touch-icon-76x76.png",
+	"favicons/apple-touch-icon-114x114.png",
+	"favicons/apple-touch-icon-120x120.png",
+	"favicons/apple-touch-icon-144x144.png",
+	"favicons/apple-touch-icon-180x180.png",
+	"favicons/apple-touch-icon.png",
+	"favicons/favicon-32.png",
+	"favicons/favicon-64.png",
+	"favicons/favicon-96.png",
+	"favicons/favicon-196.png",
+	"favicons/favicon.ico",
+	"favicons/favicon.png",
 ];
 
 for (const file of myFiles) {
-  site.remoteFile(file, import.meta.resolve(`./theme/${file}`));
+	site.remoteFile(file, import.meta.resolve(`./theme/${file}`));
 }
 
 export default site;
